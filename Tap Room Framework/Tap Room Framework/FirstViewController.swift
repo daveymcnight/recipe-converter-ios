67422b4e57af76b9c38707d1c4b1d12e69cad6e9
David McNight <David.McNight@macmini201.jacksonmg.com>
2015-08-13 17:02:10 -0400

    Beer Tap

---
+++ Tap Room Framework/Tap Room Framework/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tap Room Framework
+//
+//  Created by David McNight on 8/13/15.
+//  Copyright (c) 2015 DM. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tap Room Framework/Tap Room FrameworkTests/Tap_Room_FrameworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tap_Room_FrameworkTests.swift
+//  Tap Room FrameworkTests
+//
+//  Created by David McNight on 8/13/15.
+//  Copyright (c) 2015 DM. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Tap_Room_FrameworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
